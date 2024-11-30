5de618284c35084414851af44e7a87f8426e9372
Jake D <jmdonovan0227@gmail.com>
2024-11-29 20:07:51 -0500

    Removed redux logger from App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 // Use combine reducers to combine all reducers into a root reducer that we can give to the store.
 import { applyMiddleware, createStore, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
 // use thunk for asynchronous actions such as populating a state variable that uses a fetch call
 // to update data.
 // Thunk will wait to see if any actions returns a function instead of an object
@@ -13,10 +12,9 @@ import App from './containers/App';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 // combine reducers to create a root reducer
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
